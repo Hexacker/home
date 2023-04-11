678fc696e6a3c7bcca1b19beae52438acf04cc0a
Abdelmadjid Cherfaoui <abdelmadjid.cherfaoui@gmail.com>
2023-04-11 14:48:58 -0400

    A

--- README.md
+++ README.md
@@ -1,20 +1,3 @@
-coming-soon
-======================
-[![deploy](https://github.com/arkn98/coming-soon/workflows/deploy/badge.svg)](https://github.com/arkn98/coming-soon/actions?query=workflow%3Adeploy)
-[![license](https://img.shields.io/github/license/arkn98/coming-soon)](LICENSE)
-
-A simple coming soon page, built using [create-react-app](https://github.com/facebook/create-react-app).
-
-<p align="center">
-  <img src="./docs/scrot.png" alt="Coming soon page" width="738">
-</p>
-
-**Note:** I created this as an exercise when I was learning React. So please be mindful of many things that are considered "non-optimal".
-
-## Getting started
-
-### Installation
-
 1. Clone this repository
 2. `cd coming-soon` to go into the project root
 3. `yarn` to install dependencies
@@ -29,8 +12,13 @@ A simple coming soon page, built using [create-react-app](https://github.com/fac
 1. `yarn build` to build the app
 2. Copy the contents of the `build` folder and serve it with your own server
 
+### Serve
+
+1. `serve -s build` to serve the app
+
 ## Thanks
 
-Uses: 
- - [simple-icons](https://github.com/simple-icons/simple-icons) released under the [CC0-1.0](https://github.com/simple-icons/simple-icons/blob/develop/LICENSE.md) license.
- - [IBM Plex](https://github.com/IBM/plex) released under the [OFL-1.1](https://github.com/IBM/plex/blob/master/LICENSE.txt) license.
+Uses:
+
+- [simple-icons](https://github.com/simple-icons/simple-icons) released under the [CC0-1.0](https://github.com/simple-icons/simple-icons/blob/develop/LICENSE.md) license.
+- [IBM Plex](https://github.com/IBM/plex) released under the [OFL-1.1](https://github.com/IBM/plex/blob/master/LICENSE.txt) license.
--- src/App.js
+++ src/App.js
@@ -1,44 +1,96 @@
 import React from 'react';
-import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
+// import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
+import { ReactComponent as IconLinkedIn } from './assets/icons/linkedin.svg';
 import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
 import { ReactComponent as IconGithub } from './assets/icons/github.svg';
-import "./App.css";
+import './App.css';
 
 class App extends React.Component {
-  render = () => {
-    return (
-      <div className="card">
-        <div className="header">
-          <div className="logo">
-            <a href=".">LOGO</a>
-          </div>
-          <div className="social">
-            <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
-              <IconFacebook className="icon" />
-            </a>
-            <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
-              <IconTwitter className="icon" />
-            </a>
-            <a href="https://github.com/arkn98/coming-soon" title="GitHub" target="_blank" rel="noopener noreferrer">
-              <IconGithub className="icon" />
-            </a>
-          </div>
-        </div>
-        <div className="content">
-          <div className="title-holder">
-            <h1>Get ready for the change.</h1>
-            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
-          </div>
-          <a href="mailto:user@example.com">
-            <div className="cta">Send us an email</div>
-          </a>
-        </div>
-        <div className="footer">
-          <span>made by <a className="underlined" href="https://github.com/arkn98" target="_blank" rel="noopener noreferrer">arkn98</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/arkn98/coming-soon" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
-        </div>
-      </div>
-    );
-  }
+	render = () => {
+		return (
+			<div className="card">
+				<div className="header">
+					<div className="logo">
+						<a href=".">Hexacker</a>
+					</div>
+					<div className="social">
+						<a
+							href="https://linkedin.com/in/abdelmadjid/"
+							title="LinkedIn"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<IconLinkedIn className="icon" />
+						</a>
+						<a
+							href="https://twitter.com/Hexacker"
+							title="Twitter"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<IconTwitter className="icon" />
+						</a>
+						<a
+							href="https://github.com/Hexacker"
+							title="GitHub"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<IconGithub className="icon" />
+						</a>
+					</div>
+				</div>
+				<div className="content">
+					<div className="title-holder">
+						<h1>
+							I am a Backend Engineer. I am learning React and building my
+							website
+						</h1>
+						<p>
+							Website coming soon. Please check back to know more. Shoot me an
+							email if you're curious.
+						</p>
+					</div>
+					<a href="mailto:abdelmadjid.cherfaoui@gmail.com">
+						<div className="cta">Send me an email</div>
+					</a>
+				</div>
+				<div className="footer">
+					<span>
+						made by{' '}
+						<a
+							className="underlined"
+							href="https://github.com/Hexacker"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Abdelmadjid Cherfaoui
+						</a>{' '}
+						using{' '}
+						<a
+							className="underlined"
+							href="https://reactjs.org/"
+							title="ReactJS"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							React
+						</a>{' '}
+						|{' '}
+						<a
+							className="underlined"
+							href="https://github.com/Hexacker"
+							title="GitHub repo"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							GitHub
+						</a>
+					</span>
+				</div>
+			</div>
+		);
+	};
 }
 
 export default App;
